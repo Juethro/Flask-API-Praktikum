c06fc29efdd701357b0917f660d1737ebd8473f9
Juethroo <denisjethro@gmail.com>
2022-11-06 21:46:11 +0700

    gitignore setup

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import requests as re
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return  render_template('index.html')
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if re.method == "POST":
+        return
+    else:
+        return
 
 if __name__ == "__main__":
     app.run(debug=True)
