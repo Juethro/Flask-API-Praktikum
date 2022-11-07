bb985c6b9c7063b6f1d6b59e19e8f41798760f56
Juethroo <denisjethro@gmail.com>
2022-11-07 16:27:49 +0700

    tambahan halaman untuk web

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 import requests as re
 
 app = Flask(__name__)
@@ -7,12 +7,28 @@ app = Flask(__name__)
 def index():
     return  render_template('index.html')
 
+@app.route('/calculator', methods = ['GET', 'POST'])
+def calcul():
+    return render_template('kalku.html')
+
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    if re.method == "POST":
-        return
+    if request.method == "POST":
+        Nama = request.form['nama']
+        NIM = request.form['nim']
+        return f"Nama anda adalah {Nama} dengan nim {NIM}"
+    elif request.method == "GET":
+        return render_template('upload.html')
     else:
-        return
+        return 'HTTP methods error'
+
+@app.route('/about', methods = ['GET', 'POST'])
+def aboutme():
+    return render_template('aboutme.html')
+
+@app.route('/csvtojson', method = ['GET', 'POST'])
+def csvtojson():
+    return render_template('csvtojson.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
