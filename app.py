2e2dba69b79b50dc2824e1f050b333d94b04935a
Juethroo <denisjethro@gmail.com>
2022-11-09 09:44:10 +0700

    about me pdf reader

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request, send_file, redirect
 import json
 import csv
+import math
 
 app = Flask(__name__) #Object Declare
 deny = "Request Denied! Please do not hack this web :'("
